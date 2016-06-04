42d104a8bc69946fa54908106ac0e2b43bad356c
Ryan Renno <ryan.renno@gmail.com>
2016-06-04 00:05:22 -0400

    First FT specced out in comments, and now uses unittest.

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,25 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Djanogo' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_get_to_blog_and_verify_content(self):
+        # Melissa has heard about a cool new blog. She goes
+        # to check out its homepage
+        self.browser.get('http://localhost:8000')
+
+        # She notices the page title and header mention Ryan Renno
+        self.assertIn('Ryan Renno', self.browser.title)
+        self.fail('Finish the test!')
+
+        # She likes what she sees and leaves the site
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore') 
