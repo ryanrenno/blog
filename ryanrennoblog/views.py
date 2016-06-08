061c25cbb749bbcd622462ace28a12f27a56a994
Ryan Renno <ryan.renno@gmail.com>
2016-06-07 21:24:46 -0400

    Basic view now returns minimal HTML

--- ryanrennoblog/views.py
+++ ryanrennoblog/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
-
+from django.http import HttpResponse 
 # Create your views here.
-def home_page():
-    pass 
+def home_page(request):
+    return HttpResponse('<html><title>Ryan Renno</title></html>') 
