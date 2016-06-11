16322ed46bae851de2f242b6c07051aa47de9258
Ryan Renno <ryan.renno@gmail.com>
2016-06-10 22:20:53 -0400

    Refactoring home page view to use a tempalte

--- ryanrennoblog/tests.py
+++ ryanrennoblog/tests.py
@@ -1,4 +1,4 @@
-from django.core.urlresolvers import resolve
+from django.core.urlresolvers import resolve 
 from django.test import TestCase
 from django.http import HttpRequest
 from ryanrennoblog.views import home_page 
@@ -9,11 +9,6 @@ class HomePageTest(TestCase):
         self.assertEqual(found.func, home_page)
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
-
         response = home_page(request)
-
-        self.assertTrue(response.content.startswith(b'<html>'))
-
-        self.assertIn(b'<title>Ryan Renno</title>', response.content)
-
-        self.assertTrue(response.content.endswith(b'</html>'))
+        expected_html = render_to_string('home.html')
+        self.assertEqual(response.content.decode(),expeted_html)
